2d3e5104f2f897990af0d6ae007affd2ab69429e
s341060 <moritz.schaaf@stud-mail.uni-wuerzburg.de>
2023-01-13 18:53:59 +0100

    own files for the new functions, first documentation

--- R/aaa_util.R
+++ R/aaa_util.R
@@ -1,4 +1,12 @@
+# -----------------------------------------------------------------------------
+# specify behaviour when this package is loaded
+
+# -----------------------------------------------------------------------------
 # internal function helping with the handling of parameters
-check_input_length <- function(in1, in2 = NULL){
+check_input_length_is_1 <- function(in1){
+  if (is.null(in1) || length(in1) > 1){
+    return(FALSE)
+  }
   return(TRUE)
 }
+
